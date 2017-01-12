a2113693cc9e26270ebb78615bb4422f664191d5
Daniel Shin <shin.daniel.d@gmail.com>
2017-01-11 22:26:39 -0700

    fix socket minified file

--- config/google_auth.js
+++ config/google_auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'googleAuth' : {
         'clientId'      : '1028958173040-6fkakn3raqglbmbjmhc4l8eeelp09vgg.apps.googleusercontent.com',
         'clientSecret'  : 'sAbl0sxxRRMfa1A13gCqyY5-',
-        'callbackUrl'   : 'https://map-n-chat.herokuapp.com/'
+        'callbackUrl'   : 'https://map-n-chat.herokuapp.com/auth/google/callback'
     }
 };
