e53206d5c880988c654d979bab29bdeb48317284
Daniel Shin <shin.daniel.d@gmail.com>
2017-01-11 20:53:23 -0700

    edit oauth

---
+++ config/google_auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleAuth' : {
+        'clientId'      : '1028958173040-6fkakn3raqglbmbjmhc4l8eeelp09vgg.apps.googleusercontent.com',
+        'clientSecret'  : 'sAbl0sxxRRMfa1A13gCqyY5-',
+        'callbackUrl'   : 'http://localhost:3000/auth/google/callback'
+    }
+};
