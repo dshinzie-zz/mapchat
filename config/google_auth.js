3e1d487e6fbfd7aa0c340762cb0519c3b385b263
Daniel Shin <shin.daniel.d@gmail.com>
2017-01-11 21:04:43 -0700

    update keys

--- config/google_auth.js
+++ config/google_auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'googleAuth' : {
         'clientId'      : '1028958173040-6fkakn3raqglbmbjmhc4l8eeelp09vgg.apps.googleusercontent.com',
         'clientSecret'  : 'sAbl0sxxRRMfa1A13gCqyY5-',
-        'callbackUrl'   : 'http://localhost:3000/auth/google/callback'
+        'callbackUrl'   : 'https://map-n-chat.herokuapp.com/'
     }
 };
